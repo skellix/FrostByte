62374fcd9f9aa213087561488d5633e29b3cce80
skellix <24k911@gmail.com>
2014-01-23 02:40:41 -0700

    Fixed the LineNumberPanel

--- src/LineNumberPanel.java
+++ src/LineNumberPanel.java
@@ -1,13 +1,14 @@
-import java.awt.Component;
+import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
 import javax.swing.JTextPane;
+import javax.swing.border.EmptyBorder;
 
 
-public class LineNumberPanel extends JTextArea {
+public class LineNumberPanel extends JPanel {
 
 	private JTextPane textPane;
 	private JScrollPane jScrollPane;
@@ -15,19 +16,25 @@ public class LineNumberPanel extends JTextArea {
 	public LineNumberPanel(JTextPane textPane, JScrollPane jScrollPane) {
 		this.textPane = textPane;
 		this.jScrollPane = jScrollPane;
-		this.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 12));
+		setFont(new Font(textPane.getFont().getName(), Font.BOLD, textPane.getFont().getSize()));
 	}
 
-	@Override
-	public void update(Graphics g) {
-		String out = "";
-		int lineCount = textPane.getText().split("\n").length;
-		for (int i = 0 ; i < lineCount ; i ++) {
-			out += ""+i;
-		}
-		this.setText(out);
-		setSize((" "+lineCount).length()*getFontMetrics(getFont()).charWidth(' '), getHeight());
-		super.update(g);
+	public void paint(Graphics g) {
+		int fontHeight = getFontMetrics(textPane.getFont()).getHeight();
+		int fontWidth = getFontMetrics(textPane.getFont()).getWidths()[' '];
+		int lineStart = jScrollPane.getVerticalScrollBar().getValue() / fontHeight;
+		int lineEnd = lineStart + (int) (jScrollPane.getViewport().getSize().getHeight() / fontHeight);
+		int lineOffset = jScrollPane.getVerticalScrollBar().getValue() % fontHeight;
+		int linesSize = textPane.getText().split("(?=\n)").length;
 		
+		setBorder(new EmptyBorder(0, ((""+linesSize).length()*fontWidth)-2, 0, 0));
+		
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, ((""+lineEnd).length()+1)*fontWidth, (int) jScrollPane.getViewport().getSize().getHeight());
+		g.setColor(Color.LIGHT_GRAY);
+		int j = 1;
+		for (int i = lineStart ; i < lineEnd+1 && i < linesSize ; i ++) {
+			g.drawString(""+(i+1), 4, ((j++)*fontHeight)-lineOffset);
+		}
 	}
 }
